ef04487b14a96e565184f9914ff323c96e8d3628
ShraddhaPawar7083 <pawarshraddha201078@gmail.com>
2024-02-20 12:48:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
       {/* <Navbar /> */}
-      {/* <LeftNavbar /> */}
-      <Login/>
+      <LeftNavbar />
+      {/* <Login/> */}
       {/* <Home /> */}
       {/* <Categories/> */}
     </div>
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import "./Login.css";
@@ -13,8 +13,7 @@ function Login() {
         
     }
     return (
-        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
-            <img src="https://s3-alpha-sig.figma.com/img/4f8b/6e9d/609c7731d037f1b65d00906a7bc1e241?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Lb~DCvDWLcFkDtiNVfcZqGTXnoC7RSzqCMYJZQ~4GU-MdWV0roKSRgV3WlvM7Ovg1xwGhb9PNOzuerlNAKmkvMirm7jRGk0d4RNkOCEoJnsSCOTgXJsJW-ToggzcXjMHABCewk9rAfXUkNlkHjvP9o-dOHQHN5ZXFNkyJdfJ5VCpQ7Z1vuu6lOt0y-jHcIFP5GidiRZ0aP2L3gSc5ymrXeNLIQ8uhctZMvECoz2wHU4Nj22bwASU0aOCGesuJkk46rwUxTbd82LAkXjFAhZ7ruPMyOWN3gGkpv4Mfj37quE7ywEuesibtp6DEC7tNA4zOKPbek89cTLAcDycrvJszQ__" alt="Logo" className="logo" />
+        <div className='background-image d-flex vh-100 justify-content-center align-items-center'>
         <div className='p-3 bg-white w-35 form-container'>
                 <div className='logo-container'>
                         <img src="http://localhost:3000/images/digitalflake_logo.jpg" alt="Logo" className="logo" />
